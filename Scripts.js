1e4b47d249385bc554eb2c745ba5ee81cdeb9abe
notdraimdev <notdraimdev@gmail.com>
2024-09-28 12:46:43 +0200

    Dynamic changelog

---
+++ Scripts.js
@@ -0,0 +1,20 @@
+let a = 2;
+
+GetChangelog();
+
+async function GetChangelog() {
+    const url = "https://api.github.com/repos/notdraimdev/simplaudio/releases/latest";
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+          }
+      
+          const json = await response.json();
+          console.log(json["body"]);
+          document.getElementById("changelog").innerHTML = json["body"];
+        } catch (error) {
+          console.error(error.message);
+    }
+
+}
